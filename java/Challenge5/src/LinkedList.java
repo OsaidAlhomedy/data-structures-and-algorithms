6e11ecfb1ff27766cc5865b1f7432119921bec4d
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-10-01 14:36:36 +0300

    finished all 3 methods

--- java/Challenge5/src/LinkedList.java
+++ java/Challenge5/src/LinkedList.java
@@ -1,29 +1,65 @@
+import java.util.Objects;
+
 public class LinkedList {
 
   private Node head;
   private int size = 0;
 
   public void insert(String value) {
-    Node current = head;
+
     Node newNode = new Node(value);
+
     if (size == 0) {
-      this.head = newNode;
-    } else {
-      this.head = newNode;
+      head = newNode;
+    }
+    else {
+
+      Node current = head;
+      while(current.getNext() != null){
+        current = current.getNext();
+      }
+      current.setNext(newNode);
+
     }
     size++;
 
   }
 
-  public int size() {
-    return size;
-  }
-
   public boolean includes(String value) {
 
+    if(size != 0){
+      Node current = head;
+      while(current != null){
+        if(Objects.equals(current.getData(), value)){
+          return true;
+        }
+        current = current.getNext();
+      }
+
+    }
 
     return false;
   }
 
+  @Override
+  public String toString() {
+    StringBuilder stringPrint = new StringBuilder();
+    if(size == 0){
+      return "The list is empty";
+    }else{
+
+      Node current = head;
+      while (current != null){
+        stringPrint.append("{ ").append(current.getData()).append(" }").append(" --> ");
+        current = current.getNext();
+      }
+      stringPrint.append("NULL");
+    }
+    return stringPrint.toString();
+  }
+
+  public int size() {
+    return size;
+  }
 
 }
