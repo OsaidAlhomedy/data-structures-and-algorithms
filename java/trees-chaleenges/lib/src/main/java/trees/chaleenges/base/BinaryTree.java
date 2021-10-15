f08d4c02d60ac8687af2e92f485036aa31946c1c
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-10-15 03:36:51 +0300

    finished the challenge

---
+++ java/trees-chaleenges/lib/src/main/java/trees/chaleenges/base/BinaryTree.java
@@ -0,0 +1,87 @@
+package trees.chaleenges.base;
+
+import trees.chaleenges.data.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BinaryTree<T> {
+
+  protected Node<T> root;
+
+
+  public List<T> preOrder() {
+    if (isEmpty()) {
+      return null;
+    }
+    List<T> list = new ArrayList<>();
+    preOrderHelper(root, list);
+    return list;
+  }
+
+  public List<T> inOrder() {
+    if (isEmpty()) {
+      return null;
+    }
+    List<T> list = new ArrayList<>();
+    inOrderHelper(root, list);
+    return list;
+  }
+
+  public List<T> postOrder() {
+    if (isEmpty()) {
+      return null;
+    }
+    List<T> list = new ArrayList<>();
+    postOrderHelper(root, list);
+    return list;
+  }
+
+/////////////////////////////////////////////////////////////
+
+  private void preOrderHelper(Node<T> rootArg, List<T> list) {
+
+    list.add(rootArg.getData());
+
+    if (rootArg.getLeft() != null) {
+      preOrderHelper(rootArg.getLeft(), list);
+    }
+
+    if (rootArg.getRight() != null) {
+      preOrderHelper(rootArg.getRight(), list);
+    }
+  }
+
+  private void inOrderHelper(Node<T> rootArg, List<T> list) {
+
+    if (rootArg.getLeft() != null) {
+      inOrderHelper(rootArg.getLeft(), list);
+    }
+
+    list.add(rootArg.getData());
+
+    if (rootArg.getRight() != null) {
+      inOrderHelper(rootArg.getRight(), list);
+    }
+  }
+
+  private void postOrderHelper(Node<T> rootArg, List<T> list) {
+
+
+    if (rootArg.getLeft() != null) {
+      postOrderHelper(rootArg.getLeft(), list);
+    }
+
+    if (rootArg.getRight() != null) {
+      postOrderHelper(rootArg.getRight(), list);
+    }
+
+    list.add(rootArg.getData());
+  }
+
+
+  public boolean isEmpty() {
+    return root == null;
+  }
+
+}
